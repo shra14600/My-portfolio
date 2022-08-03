8a3519e1fc0ed6eef86a63d791eeb925b2ae16fc
shra14600 <104187381+shra14600@users.noreply.github.com>
2022-08-03 17:45:22 +0530

    my portfolio

---
+++ script.js
@@ -0,0 +1,24 @@
+$("button").click(function(){
+    $('html,body').animate({
+        scrollTop:$(".seccond").oofset().top},slow);
+});
+$(window).scroll(function(){
+    $('html,body').animate({
+        scrollTop:$("#about").offset().top},2000);
+});
+$("button").click(function(){
+    $('html,body').animate({
+        scrollTop:$(".seccond").oofset().top},slow);
+});
+$(window).scroll(function(){
+    $('html,body').animate({
+        scrollTop:$("#skill").offset().top},2000);
+});
+$(window).scroll(function(){
+    $('html,body').animate({
+        scrollTop:$("#project").offset().top},2000);
+});
+$(window).scroll(function(){
+    $('html,body').animate({
+        scrollTop:$("#contact").offset().top},2000);
+});
